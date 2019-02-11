d3d8c7c5d984791d919c6325ff9060d956df7036
Corey Lutrick <coreylutrick@gmail.com>
2019-02-11 12:08:01 -0800

    added user model and dataaccess for users

---
+++ SmashTiers/DataAccess/UserAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using SmashTiers.Models;
+using System.Data.SqlClient;
+
+namespace SmashTiers.DataAccess
+{
+    public class UserAccess
+    {
+        private string connectionString;
+
+        public UserAccess(IConfiguration config)
+        {
+            connectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public bool AddUser(User user)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var result = connection.Execute(@"INSERT INTO User (FirstName,LastName,UserName,IsActive,FirebaseId)
+                                                    VALUES (@FirstName,@LastName,@IsActive,@FireBaseId);", user);
+
+                return result == 1;
+            }
+        }
+
+    }
+}
