6355afe8b7e61b872994050770b911323abf6b22
Corey Lutrick <coreylutrick@gmail.com>
2019-02-11 12:17:40 -0800

    added user controller in for adding a new user

---
+++ SmashTiers/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmashTiers.Models;
+using SmashTiers.DataAccess;
+using Microsoft.Extensions.Configuration;
+
+namespace SmashTiers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserAccess _storage;
+
+        public UserController(IConfiguration config)
+        {
+            _storage = new UserAccess(config);
+        }
+
+        [HttpPost("addUser")]
+        public IActionResult AddNewUser(User user)
+        {
+            return Ok(_storage.AddUser(user));
+        }
+
+    }
+}
