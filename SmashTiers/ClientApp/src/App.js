3d61525292ef4f529d873e26a867bfdf535699ef
Corey Lutrick <coreylutrick@gmail.com>
2019-02-11 13:17:09 -0800

    added in firebase constants and registration/authentication routes

--- SmashTiers/ClientApp/src/App.js
+++ SmashTiers/ClientApp/src/App.js
@@ -14,6 +14,8 @@ import { StageLeastPlatforms } from './components/StageLeastPlatforms/StageLeast
 import { StageMostPlatforms } from './components/StageMostPlatforms/StageMostPlatforms';
 import { StagesTournamentLegal } from './components/StageTournamentLegal/StageTournamentLegal';
 import { StagesNotTournamentLegal } from './components/StageNotTournamentLegal/StageNotTournamentLegal';
+import { Registration } from './components/Registration/Registration';
+import { Authentication } from './components/Authentication/Authentication';
 
 export default class App extends Component {
   displayName = App.name
@@ -32,6 +34,8 @@ export default class App extends Component {
           <Route path='/character/heaviest' component={HeaviestCharacters} />
           <Route path='/stage/:id' component={SingleStage} />
           <Route path='/character/:id' component={SingleCharacter} />
+          <Route path='/registration' component={Registraion} />
+          <Route path='/authentication' component={Authentication} />
           <Route exact path='/' component={Home} />
           <Route path='/character' component={Character} />
           <Route path='/stage' component={Stage} />
