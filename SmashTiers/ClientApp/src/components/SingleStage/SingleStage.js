26590d57ca24e138a65b1b9a63a7d903fd7edf85
Corey Lutrick <coreylutrick@gmail.com>
2019-02-14 17:47:05 -0800

    added more styling

--- SmashTiers/ClientApp/src/components/SingleStage/SingleStage.js
+++ SmashTiers/ClientApp/src/components/SingleStage/SingleStage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import stageRequests from '../../DBRequests/StageRequest';
 import { Modal, Button, Glyphicon } from 'react-bootstrap';
+import { List, Card, Image } from 'semantic-ui-react'
 
 export class SingleStage extends Component {
 
@@ -28,22 +29,37 @@ export class SingleStage extends Component {
     const singleStage = this.state.stage.map((stage) =>
     {
       return (
-        <div key={stage.id}>
-          <div>
-            <div>
-              <h1>{stage.stageName}</h1>
-            </div>
-            <div>
-              <img src={stage.picture} width="350" />
-            </div>
-          </div>
+        <div key={stage.id} className="col-sm-4 col-sm-offset-4 stage">
+          <Card>
+            <Image src={stage.picture} />
+            <Card.Content>
+              <Card.Header>{stage.stageName}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <Card.Description><h4>Platforms:</h4> {stage.platforms}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Card.Description><h4>Tournament Legal:</h4> {stage.tournamentLegal === true ? "Yes" : "No"}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Card.Description>
+                <h4>Blast Zones:</h4>
+                <List>
+                  <List.Item><h5>Top:</h5> {stage.blastZoneTop}</List.Item>
+                  <List.Item><h5>Bottom:</h5> {stage.blastZoneBottom}</List.Item>
+                  <List.Item><h5>Right:</h5> {stage.blastZoneRight}</List.Item>
+                  <List.Item><h5>Left:</h5> {stage.blastZoneLeft}</List.Item>
+                </List>
+              </Card.Description>
+            </Card.Content>
+          </Card>
         </div>
       );
     });
 
     return (
       <div>
-        <div>
+        <div className="row">
           {singleStage}
         </div>
       </div>
